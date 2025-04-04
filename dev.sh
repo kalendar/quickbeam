bd5055065c0b17de7c3021b1f10f53740f1837a8
David Wiley <david.wiley@gmail.com>
2025-04-04 08:06:55 -0400

    Improvements to dev.sh

--- dev.sh
+++ dev.sh
@@ -11,8 +11,18 @@ if [ ! -f ".git/hooks/pre-commit" ]; then
     pre-commit install
 fi
 
-# Start tailwindcss, can't use same terminal because fastapi dev kills it on reload.
-npx tailwindcss -i ./src/input.css -o ./static/main.css -w
+# Start tailwindcss in the background
+npx tailwindcss -i ./src/input.css -o ./static/main.css -w &
+TAILWIND_PID=$!
+
+# Cleanup function
+cleanup() {
+    echo "Stopping Tailwind CSS watcher..."
+    kill $TAILWIND_PID
+}
+
+# Set up trap to clean up on script exit
+trap cleanup EXIT
 
 # Start FastAPI server
 fastapi dev --port 5081
